67708d529bc555d1172d2b7ac94ecc262adc9791
SuaDev09 <francisco.suarez.bastidas@uabc.edu.mx>
2022-07-15 00:23:37 -0700

    Agregar archivos de proyecto.

---
+++ consumo-de-api/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="consumo_de_api.MvcApplication" Language="C#" %>
---
+++ consumo-de-api/Models/CharacterModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace consumo_de_api.Models
+{
+    public class CharacterModel
+    {
+        /*
+        
+        
+
+        */
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string nickname { get; set; }
+        public string birthday { get; set; }
+        public List<string> occupation { get; set; }
+        public string status { get; set; }
+    }
+}
